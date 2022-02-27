7678eacdd2ca862c049a4b7b8c2343a30f45e101
Leo-paul <leopaulmartin@hotmail.fr>
2022-02-27 22:46:34 +0100

    temp get random question

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./App.css";
+
+import { GRAPHQL_CONFIG } from "./graphql_variables.js";
+import { GET_ALL_QUESTIONS } from "./gqlQueries";
+
+const createApolloClient = (authToken) => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: authToken.url,
+      headers: {
+        "x-hasura-admin-secret": authToken.secret,
+      },
+    }),
+    cache: new InMemoryCache(),
+  });
+};
 
 const CHAMPION_KEYS_URL =
   "http://ddragon.leagueoflegends.com/cdn/9.3.1/data/en_US/champion.json";
@@ -20,30 +41,39 @@ function App() {
   const [reload, setReload] = useState(false);
   const [choice1, setChoice1] = useState({});
   const [choice2, setChoice2] = useState({});
+  const [result1, setResult1] = useState("0%");
+  const [result2, setResult2] = useState("0%");
   const [question, setQuestion] = useState({});
+  const [questions, setQuestions] = useState([]);
   const [nextStep, setNextStep] = useState(false);
+  const [client] = useState(createApolloClient(GRAPHQL_CONFIG));
 
-  const dataQ = [
+  let dataQ = [
     {
       id: 0,
       question: "Which champ do you prefere on top lane?",
-    },
+      champions: [],
+    } /*,
     {
       id: 1,
       question: "Which champ do you prefere on jungle?",
-    },
+      champions: [],
+    }, 
     {
       id: 2,
       question: "Which champ do you prefere on mid lane?",
+      champions: [],
     },
     {
       id: 3,
       question: "Which champ do you prefere on bottom adc lane?",
+      champions: [],
     },
     {
       id: 4,
       question: "Which champ do you prefere on bottom support lane?",
-    },
+      champions: [],
+    },*/,
   ];
 
   async function getLOLChampionData() {
@@ -59,16 +89,24 @@ function App() {
       });
   }
 
+  async function getQuestions() {
+    await client.query({ query: GET_ALL_QUESTIONS }).then((response) => {
+      setQuestions(response.data.question);
+    });
+  }
+
   useEffect(() => {
     getLOLChampionData();
+    getQuestions();
   }, []);
 
   useEffect(() => {
-    setQuestion(dataQ[getRandomInt(dataQ.length)]);
+    //    setQuestion(dataQ[getRandomInt(dataQ.length)]);
   }, []);
 
   useEffect(() => {
     let keys = Object.keys(champions);
+    keys = keys.slice(0, 2);
     if (keys.length === 0) {
       console.error("keys empty");
       return;
@@ -83,66 +121,128 @@ function App() {
 
     setChoice1(champions[keys[randomChoice1]]);
     setChoice2(champions[keys[randomChoice2]]);
+
+    setQuestion(questions[getRandomInt(questions.length)]);
   }, [reload]);
 
   function chooseChampion(champion) {
     // cancel multiple reload before clicking on next button navigation
     if (!nextStep) setNextStep(!nextStep);
-
+    /*
     // store champ/question data
+    const idQ = question.id;
+    const currentQ = dataQ[idQ];
+    // champion list not empty
+    if (currentQ?.champions?.length > 0) {
+      // get current champions key list
+      let championKeys = Object.keys(currentQ.champions);
+      //check my current champion is in the list or not
+      const championIndex = championKeys.indexOf(champion.name);
+      if (championIndex > 0) {
+        // add 1
+        dataQ[idQ].champions[championIndex].count =
+          dataQ[idQ].champions[championIndex].count + 1;
+      } else {
+        // add new champ
+        dataQ[idQ].champions?.push({
+          name: champion.name,
+          count: 1,
+        });
+      }
+    } else {
+      // add new champ
+      dataQ[idQ]?.champions?.push({
+        name: champion.name,
+        count: 1,
+      });
+    }
 
     // display results
+    console.log(dataQ);
+    const total1 = dataQ[idQ].champions[choice1.name]?.count;
+    const total2 = dataQ[idQ].champions[choice2.name]?.count;
+    if (total1 === 0) {
+      setResult1(0 + "%");
+      setResult2(100 + "%");
+      return;
+    } else if (total2 === 0) {
+      setResult1(100 + "%");
+      setResult2(0 + "%");
+      return;
+    } else {
+      const percentRes1 = (total1 / (total1 + total2)) * 100;
+      setResult1(percentRes1 + "%");
+      setResult2(100 - percentRes1 + "%");
+    }*/
   }
 
-  function next() {
+  function next(e) {
+    e.preventDefault();
     setReload(!reload);
     setNextStep(!nextStep);
+    setResult1("");
+    setResult2("");
   }
 
   return (
-    <div className="app">
-      <div className="champions">
-        <div className="champ_choice">
-          <div className="champ_left">
-            <div className="champ_image">
-              <img
-                src={CHAMPION_PORTRAIT_URL(PATCH_VERSION, choice1?.key)}
-                alt="champion1"
-              />
+    <ApolloProvider client={client}>
+      <div className="app">
+        <div className="champions">
+          <div className="champ_choice">
+            <div className="champ_left">
+              <div className="champ_image">
+                <img
+                  src={CHAMPION_PORTRAIT_URL(PATCH_VERSION, choice1?.key)}
+                  alt="champion1"
+                />
+              </div>
+              <button
+                onClick={() => chooseChampion(choice1)}
+                className="champion_name"
+              >
+                {choice1?.name}
+              </button>
+              <div className="result">{result1}</div>
             </div>
-            <button
-              onClick={() => chooseChampion(choice1)}
-              className="champion_name"
-            >
-              {choice1?.name}
-            </button>
-          </div>
-          <div className="champ_right">
-            <div className="champ_image">
-              <img
-                src={CHAMPION_PORTRAIT_URL(PATCH_VERSION, choice2?.key)}
-                alt="champion2"
-              />
+            <div className="champ_right">
+              <div className="champ_image">
+                <img
+                  src={CHAMPION_PORTRAIT_URL(PATCH_VERSION, choice2?.key)}
+                  alt="champion2"
+                />
+              </div>
+              <button
+                onClick={() => chooseChampion(choice2)}
+                className="champion_name"
+              >
+                {choice2?.name}
+              </button>
+              <div className="result">{result2}</div>
             </div>
-            <button
-              onClick={() => chooseChampion(choice2)}
-              className="champion_name"
-            >
-              {choice2?.name}
-            </button>
           </div>
-        </div>
-        <div className="nav">
-          {nextStep && (
-            <button className="next" onClick={() => next()}>
-              Next
-            </button>
-          )}
-          {!nextStep && <div className="question">{question.question}</div>}
+          <div className="nav">
+            {nextStep && (
+              <button className="next" onClick={(event) => next(event)}>
+                Next
+              </button>
+            )}
+            {!nextStep && <div className="question">{question.question}</div>}
+          </div>
         </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
+/*const RandomQuestion = () => {
+  const { loading, error, data } = useQuery(GET_ALL_QUESTIONS);
+  if (loading) {
+    return "Loading ...";
+  }
+  if (error) {
+    return "Error: " + error;
+  }
+  const question = data.question[getRandomInt(data.question.length)].question;
+  return question;
+};*/
 
 export default App;
